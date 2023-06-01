f3f7086367d0c9f86e162804b371cd381fd7b2cd
kayjeee <kagiso.killagram@gmail.com>
2023-06-01 13:13:48 +0200

    lost nav, added like functionalty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './style.css';
-import { fetchTVShows } from '../modules/utils';
-fetchTVShows()
+import { movies } from '../modules/utils.js';
+import liking_a_Movie from '../modules/liking_a_Movie.js';
+
+const body = document.querySelector('body');
+movies();
+body.addEventListener('click', liking_a_Movie);
