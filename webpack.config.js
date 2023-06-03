6a471ad468cb46f822c84e656770a864fecbab09
kayjeee <kagiso.killagram@gmail.com>
2023-06-03 17:41:59 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,14 @@ module.exports = {
   ],
   devtool: 'source-map',
   devServer: {
-    static: './dist',
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    publicPath: '',
   },
   module: {
     rules: [
